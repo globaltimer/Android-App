763350c7f8d99d7c620d30d424ce65ef2f58ca91
Saayaman <ayakosayama1987@gmail.com>
2017-01-25 19:50:56 -0800

    listView追加

--- app/src/main/java/honkot/gscheduler/model/CompareLocale.java
+++ app/src/main/java/honkot/gscheduler/model/CompareLocale.java
@@ -4,8 +4,6 @@ import com.github.gfx.android.orma.annotation.Column;
 import com.github.gfx.android.orma.annotation.PrimaryKey;
 import com.github.gfx.android.orma.annotation.Table;
 
-import java.util.HashMap;
-
 /**
  * Created by hiroki on 2016-11-24.
  */
@@ -23,4 +21,47 @@ public class CompareLocale {
 
     @Column
     public int offset;
+
+    public CompareLocale() {
+
+    }
+
+    public CompareLocale(String id, String displayName, String GMT, int offset) {
+        this.id = id;
+        this.displayName = displayName;
+        this.GMT = GMT;
+        this.offset = offset;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDisplayName() {
+        return displayName;
+    }
+
+    public void setDisplayName(String displayName) {
+        this.displayName = displayName;
+    }
+
+    public String getGMT() {
+        return GMT;
+    }
+
+    public void setGMT(String GMT) {
+        this.GMT = GMT;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
 }
