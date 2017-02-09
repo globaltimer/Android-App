1e5550743d8d4652e67832e7ae6f98b3a84d2c61
Hiroki.H <hon87kot12.xp@gmail.com>
2017-02-08 23:24:53 -0800

    Built in data binding with RecycleView

--- app/src/main/java/honkot/gscheduler/MyRecAdapter.java
+++ app/src/main/java/honkot/gscheduler/MyRecAdapter.java
@@ -2,67 +2,56 @@ package honkot.gscheduler;
 
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import honkot.gscheduler.databinding.ListRowBinding;
 import honkot.gscheduler.model.CompareLocale;
 
 /**
  * Created by ayako_sayama on 2017/02/08.
  */
-public class MyRecAdapter extends RecyclerView.Adapter<MyRecAdapter.ViewHolder> {
+public class MyRecAdapter extends RecyclerView.Adapter<MyRecAdapter.MyViewHolder> {
 
-
-    public ArrayList<CompareLocale> compareLocale;
+    private ArrayList<CompareLocale> mData;
 
     public MyRecAdapter(ArrayList<CompareLocale> compareLocale) {
-        this.compareLocale = compareLocale;
+        mData = compareLocale;
     }
 
-//    public MyRecAdapter  r(Context applicationContext, int list_row, ArrayList<CompareLocale> worldTimes) {
-//    }
-
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-
-        View itemLayoutView = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_row, parent, false);
-        RecyclerView.ViewHolder viewHolder = new ViewHolder(itemLayoutView);
-        return (ViewHolder) viewHolder;
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        ListRowBinding itemBinding = ListRowBinding.inflate(layoutInflater, parent, false);
+        return new MyViewHolder(itemBinding);
     }
 
 
     // inner class to hold a reference to each item of RecyclerView
-    public static class ViewHolder extends RecyclerView.ViewHolder {
+    class MyViewHolder extends RecyclerView.ViewHolder {
 
-        public TextView txtViewDate;
-        public TextView txtViewTime;
-        public TextView txtViewCity;
+        private final ListRowBinding binding;
 
-
-        public ViewHolder(View itemLayoutView) {
-            super(itemLayoutView);
-
-            txtViewDate = (TextView) itemLayoutView.findViewById(R.id.dateTextView);
-            txtViewTime = (TextView) itemLayoutView.findViewById(R.id.timeTextView);
-            txtViewCity = (TextView) itemLayoutView.findViewById(R.id.cityTextView);
+        private MyViewHolder(ListRowBinding binding) {
+            super(binding.getRoot());
+            this.binding = binding;
         }
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder viewHolder, int position) {
-
-        viewHolder.txtViewDate.setText(compareLocale.get(position).getDisplayCity());
-        viewHolder.txtViewTime.setText(compareLocale.get(position).getDisplayTime());
-        viewHolder.txtViewDate.setText(compareLocale.get(position).getDisplayDate());
-
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        CompareLocale item = getItemForPosition(position);
+        holder.binding.setCompareLocale(item);
     }
 
     @Override
     public int getItemCount() {
-        return compareLocale.size();
+        return mData.size();
+    }
+
+    private CompareLocale getItemForPosition(int position) {
+        return mData.get(position);
     }
 
 }
